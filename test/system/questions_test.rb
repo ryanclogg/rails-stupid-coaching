989aef5a81435adc65f8a3dbd18b96580401562d
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-10 12:19:58 +0800

    Not too worried about the design right now, let's learn rails

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
     visit ask_url
-    assert_selector "p", text: "Ask a question here!"
+    assert_selector "p", text: "Ask your coach anything:"
   end
 end
